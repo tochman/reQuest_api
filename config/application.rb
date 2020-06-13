b9903802d329f6054156d7071fcbfc3629b597db
allezz <aerbay@gmail.com>
2020-06-13 18:47:48 +0200

    Set generators to false

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,13 @@ module ReQuestApi
                  max_age: 0
       end
     end
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+    end
   end
 end
