b018a251e14824c932d0fbc4db3c8733053a609d
japh <jenny.scherr@svenskaspel.se>
2020-06-16 16:04:59 +0200

    update model test

--- spec/factories/requests.rb
+++ spec/factories/requests.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :request do
     title { 'I need  help with this' }
     description { 'This is what I need help with' }
+    reward { 100 }
     association :requester, factory: :user
   end
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 RSpec.describe Request, type: :model do
   let(:user) { create(:user) }
 
   describe 'database table' do
     it { is_expected.to have_db_column :title }
     it { is_expected.to have_db_column :description }
+    it { is_expected.to have_db_column :reward }
   end
 
   describe 'validates' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :reward }
   end
 
   describe 'relations' do
