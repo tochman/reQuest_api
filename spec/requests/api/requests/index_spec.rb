8cf1c1c886555433e2b72dcdb54cf947a68eb2bb
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-06-16 15:31:40 +0200

    Adds request test for request index
    Co-authored-by: PaulineBA <barnades.pauline@gmail.com>

---
+++ spec/requests/api/requests/index_spec.rb
@@ -0,0 +1,41 @@
+RSpec.describe 'GET /request, can get all requests' do
+  let!(:request) { 7.times { create(:request) } }
+
+  before do
+    get '/api/requests'
+  end
+
+  describe 'successfully gets the requests' do
+    it 'has a 200 response' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'contains all the requests' do
+      expect(response_json['requests'].length).to eq 7
+    end
+
+    describe 'has keys' do
+      it ':id' do
+        expect(response_json['requests'][0]).to have_key 'id'
+      end
+
+      it ':title' do
+        expect(response_json['requests'][0]).to have_key 'title'
+      end
+
+      it ':description' do
+        expect(response_json['requests'][0]).to have_key 'description'
+      end
+
+      it ':requester' do
+        expect(response_json['requests'][0]).to have_key 'requester'
+      end
+    end
+
+    describe 'does not have keys' do
+      it ':created_at' do
+        expect(response_json['requests'][0]).not_to have_key 'created_at'
+      end
+    end
+  end
+end
