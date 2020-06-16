a226424f42ec1fe1d4ab1e20fb322438eb579113
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-06-16 18:57:09 +0200

    adds sad path tests and offer model specs

---
+++ spec/models/offer_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe Request, type: :model do
+  let(:user) { create(:user) }
+
+  describe 'database table' do
+    it { is_expected.to have_db_column :message }
+  end
+
+  describe 'relations' do
+    it { is_expected.to belong_to :helper }
+    it { is_expected.to belong_to :request }
+  end
+
+  describe 'factory' do
+    it 'should be valid' do
+      expect(create(:offer)).to be_valid
+    end
+  end
+end
--- spec/requests/api/offers/create_spec.rb
+++ spec/requests/api/offers/create_spec.rb
@@ -1,9 +1,11 @@
 RSpec.describe 'POST /offers, user can offer to help' do
-  let(:requester) { create(:user) }
+  let(:requester) { create(:user, email: 'requester@mail.com') }
+  let(:req_creds) { requester.create_new_auth_token }
+  let(:req_headers) { { HTTP_ACCEPT: 'application/json' }.merge!(req_creds) }
   let(:helper) { create(:user) }
   let(:credentials) { helper.create_new_auth_token }
   let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
-  let(:request) { create(:request, requester: requester)}
+  let(:request) { create(:request, requester: requester) }
 
   describe 'with authentication and correct params' do
     before do
@@ -34,20 +36,65 @@ RSpec.describe 'POST /offers, user can offer to help' do
       end
 
       it 'the message' do
-        expect(@offer.message).to eq "Hi, I can help!"
+        expect(@offer.message).to eq 'Hi, I can help!'
       end
     end
   end
 
-  describe 'user has already offered to help' do
+  describe 'requester tries to offer to help himself' do
+    before do
+      post '/api/offers',
+           headers: req_headers,
+           params: { request_id: request.id, message: "Hi, I can help!" }
+    end
 
+    it 'gives an error status' do
+      expect(response).to have_http_status 401
+    end
+
+    it 'gives an error message' do
+      expect(response['message']).to eq 'You cannot make an offer on your own request!'
+    end
+  end
+
+  describe 'user has already offered to help' do
+    before do
+      2.times do
+        post '/api/offers',
+             headers: headers,
+             params: { request_id: request.id, message: 'Hi, I can help!' }
+      end
+    end
+
+    it 'gives an error status' do
+      expect(response).to have_http_status 422
+    end
+
+    it 'gives an error message' do
+      expect(response['message']).to eq 'You have already made an offer on this request!'
+    end
   end
 
   describe 'without authentication but correct params' do
+    before do
+      post '/api/offers',
+           params: { request_id: request.id, message: "Hi, I can help!" }
+    end
 
+    it 'gives an error status' do
+      expect(response).to have_http_status 401
+    end
+
+    it 'gives an error message' do
+      expect(response['errors'][0]).to eq 'You need to log in'
+    end
   end
 
-  describe 'with authenticaiton and bad params' do
-
-  end
+  # describe 'with authenticaiton and bad params' do
+  #   before do
+  #     post '/api/offers',
+  #          headers: headers,
+  #          params: { request_id: request.id, message: 'Hi, I can help!' }
+  #   end
+  # end
 end
