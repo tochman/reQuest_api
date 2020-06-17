472a63cef5fe863001801df3d9c3238e07bc4f76
allezz <aerbay@gmail.com>
2020-06-17 21:09:27 +0200

    Update tests for request model with status

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Request < ApplicationRecord
-  validates_presence_of :title, :description, :reward
+  validates_presence_of :title, :description, :reward, :status
   belongs_to :requester, class_name: 'User'
   has_many :offers
   enum status: %i[pending active completed]
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -7,12 +7,14 @@ RSpec.describe Request, type: :model do
     it { is_expected.to have_db_column :title }
     it { is_expected.to have_db_column :description }
     it { is_expected.to have_db_column :reward }
+    it { is_expected.to have_db_column :status }
   end
 
   describe 'validates' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :description }
     it { is_expected.to validate_presence_of :reward }
+    it { is_expected.to validate_presence_of :status }
   end
 
   describe 'relations' do
