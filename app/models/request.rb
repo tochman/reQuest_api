5f40b504518afcb94f1121671d7bfb7ecd4276c6
allezz <aerbay@gmail.com>
2020-06-17 17:57:52 +0200

    Add status column with enums to request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,4 +4,5 @@ class Request < ApplicationRecord
   validates_presence_of :title, :description, :reward
   belongs_to :requester, class_name: 'User'
   has_many :offers
+  enum status: %i[pending active completed]
 end
