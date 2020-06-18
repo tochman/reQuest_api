c3f035c00c798abae2e2b5aeaa7dd0478e61bc88
allezz <aerbay@gmail.com>
2020-06-18 10:38:15 +0200

    Fix PR comment

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,5 +4,5 @@ class Request < ApplicationRecord
   validates_presence_of :title, :description, :reward, :status
   belongs_to :requester, class_name: 'User'
   has_many :offers
-  enum status: %i[pending active completed]
+  enum status: { pending: 0, started: 1, active: 2, completed: 3 }
 end
