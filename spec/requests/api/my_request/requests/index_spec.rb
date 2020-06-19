7e8c3b7466490b635f9a0c1c854e11a2dae7f9c7
allezz <aerbay@gmail.com>
2020-06-19 20:00:50 +0200

    Add happy paths to test

---
+++ spec/requests/api/my_request/requests/index_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+RSpec.describe 'GET /api/my_requests/requests, users can see their list of requests', type: :request do
+  let(:user) { create(:user) }
+  let(:credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
+  let!(:request) { 3.times { create(:request, requester: user) } }
+
+  let(:user_2) { create(:user) }
+  let(:credentials_2) { user_2.create_new_auth_token }
+  let(:headers_2) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials_2) }
+
+  describe 'with authentication' do
+    before do
+      binding.pry
+      get 'api/my_requests/requests', headers: headers
+    end
+
+    describe 'successfully gets the requests' do
+      it 'has a 200 response' do
+        expect(response).to have_http_status 200
+      end
+
+      it 'contains all the requests' do
+        expect(response_json['requests'].length).to eq 3
+      end
+
+      describe 'has keys' do
+        it ':id' do
+          expect(response_json['requests'][0]).to have_key 'id'
+        end
+
+        it ':title' do
+          expect(response_json['requests'][0]).to have_key 'title'
+        end
+
+        it ':reward' do
+          expect(response_json['requests'][0]).to have_key 'reward'
+        end
+      end
+
+      describe 'does not have keys' do
+        it ':created_at' do
+          expect(response_json['requests'][0]).not_to have_key 'created_at'
+        end
+        it ':requester' do
+          expect(response_json['requests'][0]).not_to have_key 'requester'
+        end
+        it ':description' do
+          expect(response_json['requests'][0]).not_to have_key 'description'
+        end
+      end
+    end
+  end
+end
