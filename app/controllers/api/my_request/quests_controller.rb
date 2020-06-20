d4fa6e819acc8058b7764b565f75ab4afc4eca39
allezz <aerbay@gmail.com>
2020-06-20 20:01:00 +0200

    Add spec file for index

--- app/controllers/api/my_request/quests_controller.rb
+++ app/controllers/api/my_request/quests_controller.rb
@@ -2,5 +2,12 @@
 
 class Api::MyRequest::QuestsController < ApplicationController
   before_action :authenticate_user!, only: %i[index]
-  def index; end
+  def index
+    quests = Request.where(helper: current_user).order('id DESC')
+    if quests == []
+      render json: { message: 'There are no Quests to show' }, status: 404
+    else
+      render json: quests, each_serializer: MyRequest::Quest::IndexSerializer
+    end
+ end
 end
