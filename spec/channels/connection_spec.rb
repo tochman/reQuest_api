fda27b4646849683b14545ec86d3035c237b3851
Thomas <thomas@craftacademy.se>
2020-06-27 11:55:13 +0200

    adds specs for connection and channel adds current user identifier to connection

---
+++ spec/channels/connection_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe ApplicationCable::Connection, type: :channel do
+  let(:user) { create(:user) }
+  it 'successfully connects' do
+    connect "/cable?uid=#{user.id}"
+    expect(connection.current_user).to eq user
+  end
+
+  it 'rejects connection' do
+    expect { connect '/cable' }.to have_rejected_connection
+  end
+end
--- spec/channels/offer_conversation_channel_spec.rb
+++ spec/channels/offer_conversation_channel_spec.rb
@@ -3,18 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe OfferConversationChannel, type: :channel do
+  let(:user) { create(:user) }
   describe 'subscription' do
+    before { stub_connection current_user: user }
     describe 'valid parameters' do
-      it {
+      before do
         subscribe(data: { offer_id: 234 })
+      end
+
+      it {
         expect(subscription).to be_confirmed
+      }
+
+      it {
+        expect(subscription).to have_stream_from("offer_conversation_234")
       }
     end
 
     describe 'without valid params' do
+
       before do
         subscribe(data: {})
       end
+
       it { expect(subscription).to be_rejected }
 
       it {
